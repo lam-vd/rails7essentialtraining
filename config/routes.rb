a29505988f5ac3277fe4bf218d897ab0627150b0
lamvd <vuduclam2412@gmail.com>
2024-01-04 00:07:52 +0700

    Edit routes root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'main#index'
+  match "about", to: "main#index", via: :get # with HTTP Verb Constraints
   get 'tasks/index'
   get 'tasks/new'
   get 'tasks/edit'

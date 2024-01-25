db128286343eeb45b603a6d37092a64ac4abd26a
lamvd <vuduclam2412@gmail.com>
2024-01-25 20:30:16 +0700

    Done Recognizing code Controller and View

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :task_find_by, only: [:show, :edit, :update, :delete, :destroy]
+
   def index
     @tasks = Task.order(:position)
   end
@@ -7,20 +9,51 @@ class TasksController < ApplicationController
   end
 
   def new
+    @count = Task.count
+    @task = Task.new(position: @count +1)
   end
 
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit
+    end
   end
 
   def delete
   end
 
   def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+  
+  def task_find_by
+    @task = Task.find_by(id: params[:id])
+  end
+  
+
+  def task_params
+    params.require(:task).permit(
+      :name,
+      :position,
+      :completed,
+      :description
+      )
   end
 end
